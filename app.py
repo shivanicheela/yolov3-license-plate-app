8f7235af3cb9a819a71cd2d1e90bb71a3d57a625
shivanicheela <155736097+shivanicheela@users.noreply.github.com>
2025-05-30 00:32:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     image_path = "temp.jpg"
     image.save(image_path)
@@ -23,4 +23,4 @@ if uploaded_file is not None:
 
     # Show prediction result
     result_img_path = os.path.join(results[0].save_dir, "temp.jpg")
-    st.image(result_img_path, caption="Detected License Plate", use_column_width=True)
+    st.image(result_img_path, caption="Detected License Plate", use_container_width=True)
